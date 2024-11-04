49cd369f8c70e8e2b2160a0ae82a1d771aa811b8
TheMagmaPsychic <156372829+TheMagmaPsychic@users.noreply.github.com>
2024-11-04 16:01:53 -0700

    Character changes works

--- Global.gd
+++ Global.gd
@@ -1,9 +1,19 @@
 extends Node
 
-var timers = {}
+var timers = {
+	running = 0
+}
 var timescale : float = 1.0
+var is_printing_warning:bool = false
+var is_printing_info:bool = false
+enum urgencies {
+	ERROR,
+	WARNING,
+	INFO
+}
 
 func _process(delta):
+	timers.running += delta
 	for repititions in range(floor(timescale)):
 		manual_process(delta, delta)
 	if timescale - floor(timescale) != 0:
@@ -21,3 +31,11 @@ func manual_process(delta, original_delta):
 
 func manual_physics_process(delta, original_delta):
 	$"/root/Main/Signal_Bus".emit_signal("physics_process", delta, original_delta)
+
+func output(text: String, urgency: urgencies = urgencies.WARNING, do_print: bool = false):
+	if urgency == urgencies.ERROR:
+		print("ERROR | ", timers.running, " | ", text)
+	if urgency == urgencies.WARNING and is_printing_warning:
+		print("WARNING | ", timers.running, " | ", text)
+	if urgency == urgencies.WARNING and is_printing_info and do_print:
+		print("INFO | ", timers.running, " | ", text)
--- player_controller.gd
+++ player_controller.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
-var speed: float = 5 #How much speed to add to velocity, not max speed
-var max_speed: float = 10
+var speed: float = 3 #How much speed to add to velocity, not max speed
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity") #We can change the gravity in project settings so it's global
 var gravity_strength: float = 0 #How much gravity is currently affecting the player
 var jump_strength: float = 0.5
@@ -11,6 +10,9 @@ var joystick_sensitivity: Dictionary = {
 	y = 0.09
 }
 var sprint_multiplier: float = 1.6 #Value multiplied to the speed when running forward.
+var print_velocity: bool = false
+var print_friction: bool = false
+var print_input:bool = false
 
 var input:Vector2 = Vector2(0, 0)
 var movement_dir:Vector3 = Vector3(0, 0, 0)
@@ -41,7 +43,7 @@ func _input(event):
 	if event.is_action_pressed("Toggle Sprint"):
 		sprint_toggled = !sprint_toggled
 
-func _physics_process(_delta): #if going faster than 100%, doesn't recalculate input on each process
+func _physics_process(delta): #if going faster than 100%, doesn't recalculate input on each process
 	#The player will only get ~70% of the sprint bonus if also strafing because of vector normalization
 	#If using a joystick, the player can be "sprinting" at very slow movement speeds
 	var look_input = Input.get_vector("Look Left", "Look Right", "Look Down", "Look Up", 0.2)
@@ -51,6 +53,7 @@ func _physics_process(_delta): #if going faster than 100%, doesn't recalculate i
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 	input = Input.get_vector("Move Left", "Move Right", "Move Forward", "Move Backward")
 	is_sprinting = Input.is_action_pressed("Sprint") or sprint_toggled
+	velocity.y += -gravity * delta
 
 func manual_physics_process(delta, original_delta):
 	if is_sprinting and input.y < 0: #if shift and w are being held, sprint only in the direction you're looking
@@ -63,29 +66,48 @@ func manual_physics_process(delta, original_delta):
 		velocity.z += movement_dir.z * speed * -1
 		if Input.is_action_just_pressed("Jump"):
 			jump()
-	else: #this is meant to give you a little control over your midair movement, but can make you go really fast too
-		velocity.x += movement_dir.x * speed * -0.1
-		velocity.z += movement_dir.z * speed * -0.1
+	var normalized_speed = Vector3(velocity.x, 0, velocity.z).normalized()
 	if is_on_floor() : #Friction
-		velocity.x *= 0.6
-		velocity.z *= 0.6
-	else:
-		velocity.x *= 0.9
-		velocity.z *= 0.9
-	var normalized_speed = Vector2(velocity.x, velocity.z).normalized()
-	if abs(velocity.x * normalized_speed.x) + abs(velocity.z * normalized_speed.y) <= 4:
+		var friction = get_friction()
+		velocity.x *= friction
+		velocity.z *= friction
+	var movement_speed = abs(velocity.x * normalized_speed.x) + abs(velocity.z * normalized_speed.z)
+	if !is_on_floor():
+		var movement_dir_normal = movement_dir.normalized()
+		var difference = normalized_speed - movement_dir_normal
+		#if difference is above_min() and difference is below_max():
+			#movement_speed *= 0.9
+		#TODO make this work, slow down when input reverse of movement
+		normalized_speed = (normalized_speed + -0.05 * movement_dir).normalized()
+		velocity.x = movement_speed * normalized_speed.x
+		velocity.z = movement_speed * normalized_speed.z
+		
+	if movement_speed <= 4:
 		sprint_toggled = false
-	#print(normalized_speed)
-	#print(Vector2.RIGHT.rotated(normalized_speed.angle()))
+	#TODO air braking
 	
-	#TODO air movement
-	
-	print("speed: " + str(abs(velocity.x * normalized_speed.x) + abs(velocity.z * normalized_speed.y)))
-	velocity.y += -gravity * delta
 	velocity.x *= delta / original_delta
 	velocity.z *= delta / original_delta
-	#velocity.y *= delta
 	move_and_slide()
+
+func get_friction():
+	var floors: Array = $FloorCollider.get_overlapping_bodies()
+	var most_friction: float = 1
+	floors.erase(self)
+	match len(floors):
+		0:
+			Global.output("Defaulting to 0.6", Global.urgencies.WARNING, print_friction)
+			return(0.6)
+		1:
+			Global.output(str(floors[0].physics_material_override.friction), Global.urgencies.INFO, print_friction)
+			return(floors[0].physics_material_override.friction)
+		_:
+			for x in floors:
+				var current_friction = x.physics_material_override.friction
+				if current_friction <= most_friction:
+					most_friction = current_friction
+	Global.output(str(most_friction), Global.urgencies.INFO, print_friction)
+	return(most_friction)
 
 func jump():
 	velocity.y = 5
