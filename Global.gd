f54161a2e51499925a00220f6040e9266b418ef9
Knight <mx4zero@yahoo.com>
2024-11-05 15:22:42 -0500

    Adding GrayBox Scene
    Includes a WIP whitebox of the inn and a incomplete floor.

--- Global.gd
+++ Global.gd
@@ -1,19 +1,9 @@
 extends Node
 
-var timers = {
-	running = 0
-}
+var timers = {}
 var timescale : float = 1.0
-var is_printing_warning:bool = false
-var is_printing_info:bool = true
-enum urgencies {
-	ERROR,
-	WARNING,
-	INFO
-}
 
 func _process(delta):
-	timers.running += delta
 	for repititions in range(floor(timescale)):
 		manual_process(delta, delta)
 	if timescale - floor(timescale) != 0:
@@ -27,15 +17,7 @@ func _physics_process(delta):
 		manual_physics_process(delta * timescale, delta)
 
 func manual_process(delta, original_delta):
-	Signalbus.emit_signal("process", delta, original_delta)
+	$"/root/Main/Signal_Bus".emit_signal("process", delta, original_delta)
 
 func manual_physics_process(delta, original_delta):
-	Signalbus.emit_signal("physics_process", delta, original_delta)
-
-func output(text: String, urgency: urgencies = urgencies.WARNING, do_print: bool = false):
-	if urgency == urgencies.ERROR:
-		print("ERROR | ", timers.running, " | ", text)
-	if urgency == urgencies.WARNING and is_printing_warning:
-		print("WARNING | ", timers.running, " | ", text)
-	if urgency == urgencies.INFO and is_printing_info and do_print:
-		print("INFO | ", timers.running, " | ", text)
+	$"/root/Main/Signal_Bus".emit_signal("physics_process", delta, original_delta)
