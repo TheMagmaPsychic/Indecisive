0900e05e488dd81fbf32c3d4743a7cb4fa9f910a
BenKling <ben.klinghoffer@gmail.com>
2024-11-19 23:25:03 +0100

    dream1 layout

--- levels/dream_1.gd
+++ levels/dream_1.gd
@@ -3,7 +3,8 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# to hide roof in editor but see it in game
+	$Roof.show()
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
--- levels/dream_4.gd
+++ levels/dream_4.gd
@@ -2,13 +2,28 @@ extends Node3D
 
 var color_cycle: Array[Color] = [Color.RED, Color.BLUE, Color.YELLOW]
 var color_index: int = 0
-
+@onready var tree = get_tree()
 
 func _on_timer_timeout() -> void:
 	var color_tween = create_tween()
 	color_tween.tween_property($DirectionalLight3D, "light_color", color_cycle[color_index], 0.1)
-	color_index = (color_index + 1) % 3
 	$AudioStreamPlayer3D.play()
+	change_walls()
+	
+	color_index = (color_index + 1) % 3
+
+
+func change_walls():
+	match color_index:
+		0:
+			tree.call_group("Red","hide")
+			tree.call_group("Yellow","show")
+		1:
+			tree.call_group("Blue","hide")
+			tree.call_group("Red","show")
+		2:
+			tree.call_group("Yellow","hide")
+			tree.call_group("Blue","show")
 
 
 func _on_start_timer_trigger_body_entered(body: Node3D) -> void:
